f47d3b9c4e3608919e357f2b5999a927f8aea3cb
darthneel <neelp22@gmail.com>
2015-03-24 11:46:27 -0400

    basic chart working

--- public/js/controllers/students.controller.js
+++ public/js/controllers/students.controller.js
@@ -1,12 +1,21 @@
 var studentsApp = studentsApp || {};
-console.log(studentsApp);
+// console.log(studentsApp);
 studentsApp.app = studentsApp.app || angular.module('studentsApp', ['angularCharts']);
 
-var json = "{\"2015-03-12\":[{\"student_id\":48,\"name\":\"Eric Kramer\",\"completed\":false},{\"student_id\":47,\"name\":\"Adrian Lin\",\"completed\":false},{\"student_id\":45,\"name\":\"Nastassia Carmona\",\"completed\":false},{\"student_id\":44,\"name\":\"Joseph Biggica\",\"completed\":false},{\"student_id\":24,\"name\":\"Patricia Laws\",\"completed\":false},{\"student_id\":27,\"name\":\"Jill Ortenberg\",\"completed\":false},{\"student_id\":28,\"name\":\"Lisa Wells\",\"completed\":false},{\"student_id\":30,\"name\":\"Andrea Ortega-Williams\",\"completed\":false},{\"student_id\":32,\"name\":\"Crawford Forbes\",\"completed\":false},{\"student_id\":33,\"name\":\"Timoor Kurdi\",\"completed\":false},{\"student_id\":34,\"name\":\"Janine Rosen\",\"completed\":false},{\"student_id\":35,\"name\":\"Tess Shapiro\",\"completed\":false},{\"student_id\":36,\"name\":\"Jeffrey Campomanes\",\"completed\":false},{\"student_id\":37,\"name\":\"Brenda Dargan\",\"completed\":false},{\"student_id\":38,\"name\":\"Clayton Albachten\",\"completed\":false},{\"student_id\":41,\"name\":\"Conor Hastings\",\"completed\":false},{\"student_id\":42,\"name\":\"Yoshie Muranaka\",\"completed\":false},{\"student_id\":43,\"name\":\"Daniel Farber\",\"completed\":false},{\"student_id\":40,\"name\":\"Shotaro Kamegai\",\"completed\":false},{\"student_id\":25,\"name\":\"Alex Fong\",\"completed\":false},{\"student_id\":26,\"name\":\"Iris Martinez\",\"completed\":false},{\"student_id\":31,\"name\":\"Heidi Williams-Foy\",\"completed\":false},{\"student_id\":39,\"name\":\"Tejal Patel\",\"completed\":false},{\"student_id\":49,\"name\":\"Neel Patel\",\"completed\":false}]}"
-
-var hwData = JSON.parse(json);
-
 studentsApp.app.controller('StudentsController', ['$scope', '$http', function($scope, $http) {
+
+  $scope.chartType = "line";
+  $scope.config = {
+    "labels": false,
+    "title": "Music Appreciation",
+    "legend": {
+      "display": true,
+      "position": "right"
+    },
+    "innerRadius": 0,
+    "lineLegend": "traditional"
+  };
+
   // var req = {
   //   method: 'GET',
   //   url: 'http://localhost:8080/hubot/hwdata'
@@ -21,8 +30,24 @@ studentsApp.app.controller('StudentsController', ['$scope', '$http', function($s
   //     console.log(data);
   //   });
 
-  $scope.hwData = hwData;
-  console.log($scope.hwData);
+  $scope.data = {
+    series: ['Maroon 5', 'Taylor Swift', 'Biggie'],
+    data:[
+      {
+        x:"Neel", 
+        y:[8, 3, 10]
+      },
+      {
+        x:"Fritzy",
+        y:[10, 2, 6]
+      },
+      {
+        x:"Jeff",
+        y:[5, 9, 3]
+      }
+    ]
+  };
+  console.log($scope.data);
 
 
 }]);
